39020683202e2084962c1d53934cb868a43d58cd
Jomil2005 <jomilgruta22@gmail.com>
2025-05-24 06:02:21 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -10,18 +10,27 @@ Route::get('/', function () {
 Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [AuthController::class, 'login'])->name('login.user');
 
-Route::middleware('auth')->group(function () {
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+// Password reset routes
+Route::get('/forget-password', [AuthController::class, 'forgotPassword'])->name('forgot.password');
+Route::post('/forgot-password', [AuthController::class, 'forgotPasswordPost'])->name('forgot.password.post');
+Route::get('/reset-password/{token}', [AuthController::class, 'resetPassword'])->name('reset.password');
+Route::post('/reset-password', [AuthController::class, 'resetPasswordPost'])->name('reset.password.post');
+
+// Authenticated routes
+Route::middleware(['auth'])->group(function () {
+
     Route::get('/owner/dashboard', [AuthController::class, 'index'])->name('owner.dashboard');
 
-    Route::get("/change-password", [AuthController::class, "changePassword"])->name("change.password");
-    Route::post("/update-password", [AuthController::class, "changePasswordUpdate"])->name("change.password.update");
-
-    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+    Route::get('/change-password', [AuthController::class, 'changePassword'])->name('change.password');
+    Route::post('/update-password', [AuthController::class, 'changePasswordUpdate'])->name('change.password.update');
 
     Route::post('/appointment-store', [AuthController::class, 'store'])->name('appointment.store');
-});
 
-Route::get("/forget-password", [AuthController::class, "forgotPassword"])->name("forgot.password");
-Route::post("/forgot-password", [AuthController::class, "forgotPasswordPost"])->name("forgot.password.post");
-Route::get("/reset-password/{token}", [AuthController::class, "resetPassword"])->name("reset.password");
-Route::post("/reset-password", [AuthController::class, "resetPasswordPost"])->name("reset.password.post");
+    // Admin dashboard route (make sure to create the view)
+    Route::get('/admin', function () {
+        return view('admin.dashboard');
+    })->name('admin.dashboard');
+
+});
