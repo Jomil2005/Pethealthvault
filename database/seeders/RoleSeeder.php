56982caf745ef56de9eb415fdf594f9650e802b6
Jomil2005 <jomilgruta22@gmail.com>
2025-05-23 22:23:16 +0800

    Update RoleSeeder.php

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -1,13 +1,26 @@
+use Illuminate\Database\Seeder;
 use App\Models\Role;
 
-Role::firstOrCreate([
-    'name' => 'Admin'
-], [
-    'description' => 'Administrator role',
-]);
+class RoleSeeder extends Seeder
+{
+    public function run()
+    {
+        // Insert Admin with fixed ID 1
+        if (!Role::where('role_id', 1)->exists()) {
+            Role::create([
+                'role_id' => 1,
+                'name' => 'Admin',
+                'description' => 'Administrator role',
+            ]);
+        }
 
-Role::firstOrCreate([
-    'name' => 'Pet Owner'
-], [
-    'description' => 'Pet Owner role',
-]);
+        // Insert Pet Owner with fixed ID 2
+        if (!Role::where('role_id', 2)->exists()) {
+            Role::create([
+                'role_id' => 2,
+                'name' => 'Pet Owner',
+                'description' => 'Pet Owner role',
+            ]);
+        }
+    }
+}
